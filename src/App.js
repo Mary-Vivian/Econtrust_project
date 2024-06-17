7abba783f0d9a3effdbbc9ea0108eeabf8ce5d63
Minu-Ochieng <minuochieng@gmail.com>
2024-06-17 09:13:00 +0300

    add services

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@
 import Landingpage from "./Landing Page"
 import Navbar from "./Navbar";
 import About from "./About/about";
+import Services from "./services";
 function App(){
   return(
     <div>
       <Navbar/>
-      <About/>
-      
       <Landingpage/>
+      <About/>
+      <Services/>
+
+      
+   
     </div>
   )
 }
