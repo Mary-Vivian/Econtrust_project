77bcda6a2fbf86adbac8892b1ec73099040e69ec
Kur-Achan <achananyiethkur@gmail.com>
2024-06-18 13:10:49 +0300

    add feature

---
+++ src/Footer/index.js
@@ -0,0 +1,37 @@
+import './index.css';
+const Footer = () => {
+    return(
+<div className="footer">
+
+    <p id='eco'>@2024 Econ Trust</p>
+   
+    
+    <br></br>
+    <div id='attributes'>
+        <br></br>
+    <p><b>Features</b></p>
+    <p>Savings </p>
+    <p>Loans</p>
+    <p>Resources & Materials</p>
+    </div>
+    <br></br>
+    <div id='firm'>
+    <p> <b>Company</b></p>
+    <p>Team</p>
+    <p>About Us</p>
+    <p>Contact</p>
+    </div>
+    <br></br>
+    <div id='valid'>
+    <p><b>Legal</b></p>
+    <p>Privacy Policy</p>
+    <p>Terms of Use</p>
+    <p>Cookies Policy</p>
+    </div>
+    <br></br>
+    <p id='copy'>@copyrights2024</p>
+    
+</div>
+    )
+}
+export default Footer;
